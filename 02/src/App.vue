3591a9ca2bc467934d5c16900ef837f954f1ac09
TortitasT <victorgf2011@gmail.com>
2022-12-03 16:11:43 +0100

    day 2 done :D, one test fails but seems to be a test thing

---
+++ 02/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
+    <p class="text-3xl p-5">{{ joke }}</p>
+    <button @click="tellMe" name="Tell Me!"
+      class="border border-slate-300 rounded-md p-3 focus:outline-none focus:ring focus:ring-sky-300">Tell
+      Me!</button>
+    <button @click="loadJoke" name="Another"
+      class="border border-slate-300 rounded-md p-3 focus:outline-none focus:ring focus:ring-sky-300">Another</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const setup = ref("")
+const delivery = ref("")
+const joke = ref("")
+
+const loadJoke = async () => {
+  const request = await fetch('https://v2.jokeapi.dev/joke/christmas')
+  const data = await request.json()
+
+  setup.value = data.setup
+  delivery.value = data.delivery
+  joke.value = data.setup
+}
+
+const tellMe = () => {
+  joke.value = delivery.value
+}
+
+loadJoke()
+</script>
